0e4f9dcb5a6a9229a0e6d19a548455ab86f3b384
Alexey <alex967268@gmail.com>
2021-09-22 11:36:21 +0300

    make it better

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ const rooms = new Map()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use((req, res, next) => {
+    res.append("Access-Control-Allow-Origin", ["*"])
+    res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
+    res.append("Access-Control-Allow-Headers", "Content-Type")
+    next()
+})
+
 app.get("/rooms/:id", (req, res) => {
     const roomId = req.params.id
 
